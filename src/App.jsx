e8f5d8c7dc681bff21c647a0cbe19b9b45d6c0d9
abhijitmondall <abhijitmondal.cse@gmail.com>
2023-05-17 22:24:51 +0600

    Assignment-11 Project Basic Structure Added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { RouterProvider } from "react-router-dom";
+// import router from "./routes/Routes";
+// import AuthProvider from "./context/AuthProvider";
+
+const App = () => {
+  return (
+    <>
+      {/* <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider> */}
+
+      <div>Hello From Client Side</div>
+    </>
+  );
+};
+
+export default App;
