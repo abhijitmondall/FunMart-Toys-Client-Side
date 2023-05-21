e83f6f55fa1ff59c5485e750444a7a5c000097cf
abhijitmondall <abhijitmondal.cse@gmail.com>
2023-05-21 11:25:27 +0600

    Code Refactor

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Styles from "./Button.module.scss";
 
 const Button = ({ btnObj, className, children }) => {
--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Styles from "./Input.module.scss";
 
 const Input = ({ inputObj }) => {
